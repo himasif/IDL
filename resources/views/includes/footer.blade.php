ce62bf39a4c617ceaa80c2dccfee2225b5d54be8
unknown <herisetyawan233@gmail.com>
2022-04-09 05:27:34 +0700

    gantifooter

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -10,6 +10,7 @@
                         <li><a href="/faq#tanya">6282338251752 (Viqi)</a></li>
                         <li><a href="/faq#tanya">6285732442883 (Dinda)</a></li>
                         <li><a href="/faq#tanya">6285604834565 (fadil)</a></li>
+                        <li><a href="/faq#tanya">6285203870233 (Aiya)</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-4 col-md-3 item">
