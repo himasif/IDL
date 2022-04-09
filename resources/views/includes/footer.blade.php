37e627bdf0d99d38a5e0505d681a9e1642f5cabb
unknown <herisetyawan233@gmail.com>
2022-04-09 09:24:00 +0700

    update: cp

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -8,8 +8,8 @@
                         <li><a href="mailto:humas@idle-unej.my.id">E-Mail : humas@idle-unej.my.id</a></li>
                         <li><a href="/faq#tanya">6281294710595 (Alifta)</a></li>
                         <li><a href="/faq#tanya">6282338251752 (Viqi)</a></li>
-                        <li><a href="/faq#tanya">6285604834565 (fadil)</a></li>
                         <li><a href="/faq#tanya">6285203870233 (Aiya)</a></li>
+                        <li><a href="/faq#tanya">6285604834565 (fadil)</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-4 col-md-3 item">
